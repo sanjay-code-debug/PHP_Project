7525dd601168f20f0fe79dc711c77dcdc78a3bf4
Sanjay-coder-debug <sanjaykumardasprivate@gmail.com>
2021-05-17 12:46:04 +0530

    Blog Project

---
+++ Blog/Model/Database.php
@@ -0,0 +1,23 @@
+<?php 
+namespace Model;
+
+// Connection Class Making Connection 
+class Database{
+
+    public function getConnection(){
+        $host = '127.0.0.1:3308';
+        $user = 'root';
+        $pass='';
+        $dbname='blog';
+       
+        $dsn = "mysql:host=$host;dbname=$dbname";
+      
+        $conn= new  \PDO($dsn, $user, $pass);
+        return $conn;
+    }   
+}
+
+
+
+
+?>
